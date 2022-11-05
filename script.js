2a4d6a536b38adb649808d8878c8d17089428e84
Killian Quelavoine <95000365+KillianQuelavoine@users.noreply.github.com>
2022-11-05 16:55:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//----------Map crée via mapbox.com-------------------
 //Accès au token
 mapboxgl.accessToken = 'pk.eyJ1IjoibmF0aGFsaWVkcCIsImEiOiJjbDh4NjBqOWkwMXE3M3dwY292bDNzcDVoIn0.hxFDPbpzjCLJ3ZpIji1SJQ';
 //Initialisation de la map 
@@ -6,17 +7,19 @@ const map = new mapboxgl.Map({
     //Application du style 
     style: 'mapbox://styles/nathaliedp/cla2xdem9002f14pcpyzv0zd3',
     center: [3.483, 46.205], // starting position [lng, lat],
-    zoom: 4.5, // starting zoom
-    // These options control the camera position after animation
+    zoom: 4.5, // Controle du zoom
+    // Position de la camera
     pitch: 46,
     bearing: 20,
 });
 
 
+//Pour le changement de date 
 let datechange = function datechangement() {
     document.getElementById("suivante").click();
 }
 
+//-------------Animation de la carte---------------
 // Fonction pour lancer la lecture
 function play() {
     intervaldate = setInterval(datechange, 2000);
@@ -82,16 +85,24 @@ document.getElementById("date").addEventListener("input", function() {
     document.getElementById("submit").click();
 });
 
+// ---------------- Fin de l'animation-------------
+
+
+
 document.getElementById('submit').addEventListener('click', function() {
 
     document.querySelectorAll('.marker').forEach(el => el.parentNode.removeChild(el));
     let date = document.getElementById('date').value;
+    //On va ici charger ici le contenu du fichier JSON dans une variable data en utilisant fetch Javascipt
     fetch('fire_archive_M-C61_301806.json').then(function(response) {
         response.json().then(function(data) {
 
             function feu(data) {
                 data.forEach(function(f) {
+                    //Si acq_date de notre json correspond au conditions ci dessous alors : 
+                    //On met en condition l'année selectionnée + les mois d'été 
                     if (f.acq_date.substring(0, 4) == date && (f.acq_date.substring(5, 7) == 06 || f.acq_date.substring(5, 7) == 07 || f.acq_date.substring(5, 7) == 08 || f.acq_date.substring(5, 7) == 09)) {
+                        //creation des markeurs ( les feux )
                         let el = document.createElement('div')
                         el.className = 'marker';
                         el.style.width = '3px';
@@ -99,6 +110,7 @@ document.getElementById('submit').addEventListener('click', function() {
 
                         new mapboxgl.Marker(el)
                             .setLngLat([f.longitude, f.latitude])
+                            //On les rajoute a la map
                             .addTo(map);
                     }
                 });
@@ -129,7 +141,7 @@ document.getElementById('submit').addEventListener('click', function() {
     })
 })
 
-
+//Nombre de feu par année 
 let nbfeuannee = [{
     A: 2002,
     B: 1426
@@ -195,8 +207,11 @@ let nbfeuannee = [{
     B: 2208
 }];
 
+//-----------------Graph Histogramme---------------------
 let scaleY = 1 / 30;
 
+
+//Creation des axes pour l'histogramme
 const axeG =
     d3.axisLeft(d3.scaleLinear()
         .domain([0, 3000]) // la plage de valeurs possibles 
@@ -232,6 +247,8 @@ svg.append("g")
 
 let w = 150 / nbfeuannee.length;
 
+
+// Création du linear gradient appliqué sur les barres 
 let areaGradient = svg.append("defs")
     .append("linearGradient")
     .attr("id", "areaGradient")
@@ -267,7 +284,7 @@ d3.selectAll(".barres")
     .style("height", d => d.B * scaleY)
     .style("y", d => -d.B * scaleY)
     .style("x", "4")
-    .style("fill", "url(#areaGradient)")
+    .style("fill", "url(#areaGradient)") //On applique le linear gradiant qu'on a crée précédamment 
     .style("stroke", "none");
 
 d3.selectAll(".barres")
@@ -294,6 +311,12 @@ d3.selectAll(".barres")
             .remove();
     });
 
+
+// -------------------Fin de l'histogramme --------------------------
+
+//-------------Création du graph nuage de point relié par les traits--------------------
+
+//On utilise ici les données du json des moyennes de températures de 2002 à 2022
 d3.json('temperature.json').then(function(d) {
 
     let scaley = d3.scaleLinear()
@@ -411,8 +434,9 @@ d3.json('temperature.json').then(function(d) {
         })
 });
 
-// LES MENTIONS LEGALES
+//------------- Fin du graph nuage de point --------------------
 
+//-------------Les mentions légales -------------------
 
 document.querySelector('.volet-invisible').addEventListener('click', function(click) {
     //Afficher le mot "click" dans la console quand on a cliqué sur l'élément ayant pour classe volet-invisible
@@ -436,20 +460,21 @@ document.querySelector('.volet-invisible').addEventListener('click', function(cl
 // Pour refermer le volet des mentions légales : 
 
 document.getElementById('volet').addEventListener('click', function(click) {
-    // afficher le mot « click » dans la console quand on a cliqué sur l’élément ayant pour classe volet-invisible
-    console.log("click")
+        // afficher le mot « click » dans la console quand on a cliqué sur l’élément ayant pour classe volet-invisible
+        console.log("click")
 
-    // Animation au bout de 800 millisecondes après le clic
-    document.querySelector('.volet-visible').animate([{
-            height: '5em'
-        }], {
-            duration: 800
-        })
-        // Attribution de la classe volet-invisible en supprimant la classe volet-visible
-        //La classe volet-invisible étant remise, la partie des mentions légales est caché 
-    setTimeout(function() {
-        document.getElementById('volet').classList.remove('volet-visible');
-        document.getElementById('volet').classList.add('volet-invisible');
-    }, 800);
+        // Animation au bout de 800 millisecondes après le clic
+        document.querySelector('.volet-visible').animate([{
+                height: '5em'
+            }], {
+                duration: 800
+            })
+            // Attribution de la classe volet-invisible en supprimant la classe volet-visible
+            //La classe volet-invisible étant remise, la partie des mentions légales est caché 
+        setTimeout(function() {
+            document.getElementById('volet').classList.remove('volet-visible');
+            document.getElementById('volet').classList.add('volet-invisible');
+        }, 800);
 
-})
+    })
+    //------------- Fin des mentions légales -------------------
